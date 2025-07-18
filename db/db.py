639af3bceb9923cd84e60cc65a46f29cd85695a5
dfeen87 <dfeen87@gmail.com>
2025-07-18 14:21:58 -0400

    Remove outdated root-level db folder, keep src/peoples_coin/db/ as main

--- db/db.py
+++
@@ -1,5 +0,0 @@
-from flask_sqlalchemy import SQLAlchemy
-
-# This db object is the single source of truth for your database connection
-# and provides the base class for your models to inherit from (db.Model).
-db = SQLAlchemy()
