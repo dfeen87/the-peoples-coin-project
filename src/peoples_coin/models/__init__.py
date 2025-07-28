b880de17148d4d2e32a27f1a11ef237222884b82
dfeen87 <dfeen87@gmail.com>
2025-07-27 21:02:43 -0400

    new models init

--- src/peoples_coin/models/__init__.py
+++ src/peoples_coin/models/__init__.py
@@ -2,3 +2,8 @@ from sqlalchemy.orm import declarative_base
 
 Base = declarative_base()
 
+# Expose your models here:
+from .user import UserAccount, UserWallet
+from .chain import ChainBlock, ConsensusNode
+from .entry import DataEntry, EventLog, GoodwillAction
+
